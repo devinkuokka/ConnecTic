abe78b4852bc9ea71ffd3ae76464ee3cce80c0cd
Carolyn Dean Wolf <cdeanwolf@wustl.edu>
2016-04-22 00:56:05 -0500

    so far...

--- app/app.js
+++ app/app.js
@@ -1,12 +1,46 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var myApp = angular.module('myApp', [
+  'ngRoute'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/home', {
+      templateUrl: 'home.html',
+      controller: 'homeCtrl'
+    }).
+    when('/home/tictac', {
+      templateUrl: 'ticTac.html',
+      controller: 'ticTacCtrl'
+    }).
+    when('/home/connect4', {
+      templateUrl: 'connect4.html',
+      controller: 'connect4Ctrl'
+    }).
+    when('/home/blackjack', {
+      templateUrl: 'blackjack.html',
+      controller: 'blackjackCtrl'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
+  
 }]);
+
+myApp.controller('homeCtrl', function($scope) {
+  
+});
+
+myApp.controller('ticTacCtrl', function($scope) {
+  
+});
+
+myApp.controller('connect4Ctrl', function($scope) {
+  
+});
+
+myApp.controller('blackjackCtrl', function($scope) {
+  
+});
